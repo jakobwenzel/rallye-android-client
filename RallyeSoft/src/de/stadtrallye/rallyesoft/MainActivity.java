fc8f3bb596ac1eda802f94097ef96f2bf28d55e5
Ray <ramon.wirsch@stud.tu-darmstadt.de>
2012-11-01 01:29:42 +0100

    .

--- RallyeSoft/src/de/stadtrallye/rallyesoft/MainActivity.java
+++ RallyeSoft/src/de/stadtrallye/rallyesoft/MainActivity.java
@@ -1,13 +1,19 @@
 package de.stadtrallye.rallyesoft;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockActivity;
 
 import de.stadtrallye.rallyesoft.communications.PushService;
+import de.stadtrallye.rallyesoft.fragments.OverviewFragment;
 
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends SherlockActivity implements  ActionBar.OnNavigationListener {
 	
 	public PushService push;
 	
@@ -16,10 +22,37 @@ public class MainActivity extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		
 		setTitle(R.string.title_main);
-		setContentView(R.layout.main);
+//		setContentView(R.layout.overview);
 		
 		ActionBar ab = getSupportActionBar();
 		ab.setDisplayHomeAsUpEnabled(false);
-		ab.addTab(tab
+		Context context = ab.getThemedContext();
+		ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(context, R.array.tabs, R.layout.sherlock_spinner_item);
+//        list.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
+		
+		
+		ab.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        ab.setListNavigationCallbacks(list, this);
+		ab.setSelectedNavigationItem(0);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		switch (itemPosition) {
+		case 0: 
+			OverviewFragment newFragment = new OverviewFragment();
+		    FragmentTransaction ft = null;
+		    // Replace whatever is in the fragment container with this fragment
+		    //  and give the fragment a tag name equal to the string at the position selected
+		    ft.replace(R.id.fragment_container, newFragment, strings[position]);
+		    // Apply changes
+		    ft.commit();
+		break;
+		default:
+			Toast.makeText(getApplicationContext(), getResources().getString(R.string.unsupported_link), Toast.LENGTH_SHORT).show();
+		break;
+		}
+		
+		return false;
 	}
 }
