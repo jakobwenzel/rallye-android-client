19002e8fc229c235c221bc9c6c423b8c983a331d
Ray <ramon.wirsch@stud.tu-darmstadt.de>
2012-11-02 02:01:31 +0100

    test readout from hajoschja.de

--- RallyeSoft/src/de/stadtrallye/rallyesoft/communications/Pull.java
+++ RallyeSoft/src/de/stadtrallye/rallyesoft/communications/Pull.java
@@ -10,7 +10,12 @@ import java.net.URL;
 
 import org.json.JSONTokener;
 
-public class Pull {
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class Pull extends AsyncTask<String, Integer, String> {
+	
+	public String result;
 	
 	public Pull() {
 		
@@ -24,10 +29,11 @@ public class Pull {
 		String answer = null;
 		
 		try {
-			url = new URL("http://hajoschja.de:10101/rallye/nodes/get-all");
+			url = new URL("http://hajoschja.de:10101/StadtRallye/map/getAllNodes");
 			connection = (HttpURLConnection) url.openConnection();
 			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			answer = in.readLine();
+			Log.w("pull", answer);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,5 +49,19 @@ public class Pull {
 		
 		
 		return answer;
+	}
+
+	@Override
+	protected String doInBackground(String... params) {
+		
+		result = testConnection();
+		Log.w("pull3", result);
+		return result;
+	}
+	
+	@Override
+	protected void onPostExecute(String res) {
+		Log.w("pull4", res);
+		result = res;
 	}
 }
--- RallyeSoft/src/de/stadtrallye/rallyesoft/fragments/MapFragment.java
+++ RallyeSoft/src/de/stadtrallye/rallyesoft/fragments/MapFragment.java
@@ -1,13 +1,17 @@
 package de.stadtrallye.rallyesoft.fragments;
 
+import java.util.concurrent.ExecutionException;
+
 import de.stadtrallye.rallyesoft.R;
 import de.stadtrallye.rallyesoft.communications.Pull;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MapFragment extends Fragment {
 	
@@ -17,8 +21,8 @@ public class MapFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		pull.execute(new String[]{});
 		
-		text = pull.testConnection();
 	}
 	
 	@Override
@@ -28,6 +32,22 @@ public class MapFragment extends Fragment {
 	
 	@Override
 	public void onStart() {
+		super.onStart();
+		
+		
+		try {
+			text = pull.get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		Log.w("pull2", text);
+		
+		Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+		
 		TextView tv = (TextView) (getView().findViewById(R.id.placeholder));
 		tv.setText(text);
 	}
