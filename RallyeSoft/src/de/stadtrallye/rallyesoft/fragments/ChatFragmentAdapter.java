b295bbbea274e3a3c0eb38e20cf6b7c5a83cccc5
Ray <Ray@Ray-Note>
2012-12-13 23:38:04 +0100

    android.support.v4 now as project instead of a .jar Build order changed, only one instance of android.support.v4 referenced Bug fixed (App would terminate if Chat was called a second time): ->	ChatsFragment is not reused, because its ChildFragmentManager is detached from the Activity onDestroy ->	FragmentHandler has a Option to not re use Fragments Bug Found (Rotation in ChatsFragment with chatroom loaded will terminate the app): -> 	Callback from Model seems to occur before instantiation of the View hierarchy?

--- RallyeSoft/src/de/stadtrallye/rallyesoft/fragments/ChatFragmentAdapter.java
+++
@@ -1,8 +0,0 @@
-package de.stadtrallye.rallyesoft.fragments;
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-
-
