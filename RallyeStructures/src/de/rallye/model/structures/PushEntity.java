f8ef897bc8d71ccff807403207a68a2473cb9c02
Ramon Wirsch <rkit5.mail@gmail.com>
2013-06-18 19:08:32 +0200

    ~RallyeStructures

---
+++ RallyeStructures/src/de/rallye/model/structures/PushEntity.java
@@ -0,0 +1,9 @@
+package de.rallye.model.structures;
+
+public class PushEntity {
+	
+	public static final String PAYLOAD = "payload";
+	public static final String TYPE = "type";
+	
+	public enum Type { newMessage, messageChanged }
+}
