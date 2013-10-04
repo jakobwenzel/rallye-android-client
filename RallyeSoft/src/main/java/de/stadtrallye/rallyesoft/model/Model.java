6e2db14acad55e0877e22b38f727e8337fcab2f1
Jakob Wenzel <jakobwenzel92@gmail.com>
2013-10-04 19:11:55 +0200

    Correctly load user names if not found

--- RallyeSoft/src/main/java/de/stadtrallye/rallyesoft/model/Model.java
+++ RallyeSoft/src/main/java/de/stadtrallye/rallyesoft/model/Model.java
@@ -571,7 +571,8 @@ public class Model implements IModel, RequestExecutor.Callback<Model.CallbackIds
 
 	@Override
 	public void onMissingUserName(int userID) {
-		refreshAvailableGroups();
+		Log.i(THIS,"Missing User name for "+userID);
+		refreshAllUsers();
 	}
 
 	@Override
